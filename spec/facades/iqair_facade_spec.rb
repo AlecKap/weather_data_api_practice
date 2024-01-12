92af6f15c7b8b2f70d58e8e1c4b5941132da83f9
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-01-11 17:44:48 -0700

    Add weather-related methods to IqairFacade

--- spec/facades/iqair_facade_spec.rb
+++ spec/facades/iqair_facade_spec.rb
@@ -11,5 +11,33 @@ RSpec.describe IqairFacade do
         expect(@facade.location_time_date).to eq("Northglenn, Colorado at #{Time.now.strftime('%I:%M %p')} on #{Date.today.strftime('%B %d, %Y')}:")
       end
     end
+
+    describe '#nearest_city_weather' do
+      it 'returns the nearest city weather', :vcr do
+        expect(@facade.nearest_city_weather).to be_a(Weather)
+        expect(@facade.nearest_city_weather.date).to be_a(String)
+        expect(@facade.nearest_city_weather.temp).to be_a(Integer)
+        expect(@facade.nearest_city_weather.humidity).to be_a(Integer)
+        expect(@facade.nearest_city_weather.wind_speed).to be_a(Integer)
+      end
+    end
+
+    describe '#nearest_city_temp' do
+      it 'returns the nearest city temperature', :vcr do
+        expect(@facade.nearest_city_temp).to be_a(Integer)
+      end
+    end
+
+    describe '#nearest_city_humidity' do
+      it 'returns the nearest city humidity', :vcr do
+        expect(@facade.nearest_city_humidity).to be_a(Integer)
+      end
+    end
+
+    describe '#nearest_city_wind_speed' do
+      it 'returns the nearest city wind speed', :vcr do
+        expect(@facade.nearest_city_wind_speed).to be_a(Integer)
+      end
+    end
   end
 end
